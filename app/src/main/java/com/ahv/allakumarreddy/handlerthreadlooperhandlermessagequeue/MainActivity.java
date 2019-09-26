9038ec584845fba12a2341a5d6ff3b3aa430d9ed
ALLA HEMANTH VENKATA PAVAN KUMAR REDDY <ahvworkatro@gmail.com>
2019-09-26 18:34:58 +0530

    Initial commit

---
+++ app/src/main/java/com/ahv/allakumarreddy/handlerthreadlooperhandlermessagequeue/MainActivity.java
@@ -0,0 +1,57 @@
+package com.ahv.allakumarreddy.handlerthreadlooperhandlermessagequeue;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView mTextView = findViewById(R.id.tv);
+
+        final Handler mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                mTextView.setText((String) msg.obj);
+            }
+        };
+
+        MyHandlerThread mWorkerThread = new MyHandlerThread("Worker Thread");
+        mWorkerThread.start();
+        mWorkerThread.prepareLooper();
+        mWorkerThread.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message msg = mHandler.obtainMessage();
+                msg.obj = "execution completed 1";
+                mHandler.sendMessage(msg);
+            }
+        }).execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message msg = mHandler.obtainMessage();
+                msg.obj = "execution completed 2";
+                mHandler.sendMessage(msg);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ahv/allakumarreddy/handlerthreadlooperhandlermessagequeue/MyHandlerThread.java
@@ -0,0 +1,26 @@
+package com.ahv.allakumarreddy.handlerthreadlooperhandlermessagequeue;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+
+public class MyHandlerThread extends HandlerThread {
+    private Handler mHandler;
+
+    public MyHandlerThread(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mHandler = new Handler(getLooper());
+    }
+
+    public void prepareLooper(){
+        mHandler = new Handler(getLooper());
+    }
+
+    public MyHandlerThread execute(Runnable task){
+        mHandler.post(task);
+        return this;
+    }
+}
